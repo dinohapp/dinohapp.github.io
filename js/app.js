d004160220bbd25dfe6bdeff438b34c8a6eb344f
dinohapp <dinohapp@gmail.com>
2021-09-28 21:43:14 -0400

    add ava

---
+++ js/app.js
@@ -0,0 +1,3 @@
+(function(window){'use strict';var BreathingHalftone=window.BreathingHalftone;var isInited=false;function init(){if(isInited){return;}
+isInited=true;var img=document.querySelector('#hero img');new BreathingHalftone(img,{});}
+document.addEventListener('DOMContentLoaded',init,false);window.onload=init;})(window);
